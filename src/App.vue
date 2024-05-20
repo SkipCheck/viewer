316e8b3dfa17647753db4be30643f79e371452a6
encore <encoregranted@gmail.com>
2024-05-20 11:19:31 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FieldComponent :data = "FieldData"/>
+  </div>
+</template>
+
+<script>
+import FieldComponent from './components/FieldComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FieldComponent
+  },
+  data(){
+    return{
+      FieldData: "Примерный текст для вывода",
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
